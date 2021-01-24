c29d2f71379bfdddfb39b54c1b5634928c506a69
Casey Trombley <casey@algaecal.com>
2021-01-24 16:36:33 -0500

    basic setup

--- pages/about.vue
+++ pages/about.vue
@@ -7,7 +7,7 @@
 
         <SearchPopup />
 
-        <BreadcrumbOne :items="items" title="We are an agency located in New York" backgroundUrl= "/images/bg/breadcrumb-bg.jpg" />
+        <BreadcrumbOne :items="items" title="We are here to help" backgroundUrl= "/images/bg/breadcrumb-bg.jpg" />
 
         <AboutTwo class="section-padding-top about-section-padding-bottom-200" />
 
