91a21b4d2112106289eb9f709278e25ca76370e3
Casey Trombley <casey@algaecal.com>
2021-01-24 16:43:38 -0500

    basic setup

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,16 +1,16 @@
 <template>
     <nav class="site-main-menu">
-<!--        <ul>-->
-<!--            <li>-->
-<!--                <n-link to="/about" class="link">About Us</n-link>-->
-<!--            </li>-->
+        <ul>
+            <li>
+                <n-link to="/about" class="link">About Us</n-link>
+            </li>
 <!--            <li>-->
 <!--                <n-link to="/service" class="link">Services</n-link>-->
 <!--            </li>-->
 <!--            <li>-->
 <!--                <n-link to="/contact-us" class="link">Contact Us</n-link>-->
 <!--            </li>-->
-<!--        </ul>-->
+        </ul>
     </nav>
 </template>
 <style lang="scss" scoped>
